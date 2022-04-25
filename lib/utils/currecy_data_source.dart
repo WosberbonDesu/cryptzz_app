49fc47ea2b2d068161f628742727d0be3e527358
Berk-e <berkecan.pinar@sigmatelecom.com>
2022-04-25 18:15:53 +0300

    master

--- lib/utils/currecy_data_source.dart
+++ lib/utils/currecy_data_source.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_svg/svg.dart';
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 import 'package:flutter/material.dart';
 import '../models/currency.dart';
@@ -5,20 +6,69 @@ import '../models/currency.dart';
 enum CurrencyColumn { id, rank, name, price, oneHChange, oneDChange, marketCap }
 
 class CurrencyDataSource extends DataGridSource {
+  late List<DataGridRow> _currencies;
+  @override
+  List<DataGridRow> get rows => _currencies;
   CurrencyDataSource({required List<Currency> currencies}) {
     buildDataGrid(currencies);
   }
-  void buildDataGrid(List<Currency> currencies) => currencies.map<DataGridRow>(
+  void buildDataGrid(List<Currency> currencies) => _currencies = currencies
+      .map<DataGridRow>(
         (currency) => DataGridRow(
           cells: CurrencyColumn.values
-              .map((column) =>
-                  DataGridCell(columnName: column.toString(), value: currency))
+              .map((column) => DataGridCell<Currency>(
+                  columnName: column.toString(), value: currency))
               .toList(),
         ),
-      );
+      )
+      .toList();
   @override
   DataGridRowAdapter buildRow(DataGridRow row) => DataGridRowAdapter(
           cells: row.getCells().map<Widget>((dataGridCell) {
-        return Text("ss");
+        final Currency currency = dataGridCell.value;
+        final column = CurrencyColumn.values
+            .firstWhere((value) => value.toString() == dataGridCell.columnName);
+        switch (column) {
+          case CurrencyColumn.id:
+            return buildIdRow(currency);
+          case CurrencyColumn.price:
+            return buildPriceRow(currency.price);
+          case CurrencyColumn.marketCap:
+            return buildMarketCap(currency.marketCap);
+          default:
+            return Text("Hello", style: TextStyle(color: Colors.white));
+        }
       }).toList());
+
+  Widget buildIdRow(Currency currency) => Container(
+        padding: EdgeInsets.all(12.0),
+        child: Row(children: [
+          buildLogo(currency),
+          SizedBox(
+            width: 8,
+          ),
+          Text(
+            currency.id,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(color: Colors.white),
+          ),
+        ]),
+      );
+  Widget buildPriceRow(double price) => Container(
+      padding: EdgeInsets.all(16),
+      child: Text(
+        "\$${price}",
+        style: TextStyle(color: Colors.teal),
+      ));
+
+  Widget buildLogo(Currency currency) {
+    final isSvg = currency.logoUrl.endsWith(".svg");
+    return CircleAvatar(
+      backgroundColor: Colors.black,
+      child: isSvg
+          ? SvgPicture.network(currency.logoUrl)
+          : Image.network(currency.logoUrl),
+      radius: 10,
+    );
+  }
 }
