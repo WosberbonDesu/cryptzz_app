7b00eb7f5354710c6c28dbbf87571889a71bd632
Berk-e <berkecan.pinar@sigmatelecom.com>
2022-04-25 10:57:29 +0300

    master

--- lib/business/constants/colors.dart
+++ lib/business/constants/colors.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0XFF6A62B7);
-const kBackgroundColor = Color(0XFFE5E5E5);
+const kPrimaryColor = Color.fromARGB(255, 11, 8, 42);
+const kBackgroundColor = Color.fromARGB(255, 232, 232, 232);
