a99b705d410f51d5fd29d9ad069cff5945678d33
Berk-e <berkecan.pinar@sigmatelecom.com>
2022-05-09 17:19:33 +0300

    master

--- lib/ui/widgets/crypto_coin_page/coin_logo_widget.dart
+++ lib/ui/widgets/crypto_coin_page/coin_logo_widget.dart
@@ -7,8 +7,10 @@ import '../../../models/fetch_coin/data_model.dart';
 
 class CoinLogoWidget extends StatelessWidget {
   final DataModel coin;
-  const CoinLogoWidget({
+  String icon_names;
+  CoinLogoWidget({
     Key? key,
+    required this.icon_names,
     required this.coin,
   }) : super(key: key);
 
@@ -16,7 +18,7 @@ class CoinLogoWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     var coinIconUrl =
         "https://raw.githubusercontent.com/spothq/cryptocurrency-icons/master/128/color/";
-    TextTheme textStyle = Theme.of(context).textTheme;
+    //TextTheme textStyle = Theme.of(context).textTheme;
 
     return Container(
       padding: const EdgeInsets.only(left: 16.0),
@@ -29,7 +31,7 @@ class CoinLogoWidget extends StatelessWidget {
               height: 50.0,
               width: 50.0,
               child: CachedNetworkImage(
-                imageUrl: ((coinIconUrl + coin.symbol + ".png").toLowerCase()),
+                imageUrl: ((coinIconUrl + icon_names + ".png").toLowerCase()),
                 placeholder: (context, url) => CircularProgressIndicator(),
                 errorWidget: (context, url, error) =>
                     SvgPicture.asset('assets/icons/dollar.svg'),
@@ -37,13 +39,16 @@ class CoinLogoWidget extends StatelessWidget {
           const SizedBox(height: 4.0),
           Text(
             coin.symbol,
-            style: textStyle.subtitle1,
+            style: TextStyle(color: Colors.red, fontSize: 10),
           ),
           const SizedBox(height: 2.0),
-          Text(
-            "\$" + coin.quoteModel.usdModel.price.toStringAsFixed(2),
-            style: textStyle.subtitle2,
-          ),
+          RichText(
+              text: TextSpan(children: [
+            TextSpan(text: "\$", style: TextStyle(color: Colors.greenAccent)),
+            TextSpan(
+                text: coin.quoteModel.usdModel.price.toStringAsFixed(2),
+                style: TextStyle(color: Colors.white, fontSize: 15))
+          ])),
         ],
       ),
     );
