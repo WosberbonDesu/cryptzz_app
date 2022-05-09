591aca957dbfa53fdc34f8727b57c55c77638975
Berk-e <berkecan.pinar@sigmatelecom.com>
2022-05-09 14:17:51 +0300

    master

--- lib/ui/pages/tab_pages/pages/crypto_coins_showing.dart
+++ lib/ui/pages/tab_pages/pages/crypto_coins_showing.dart
@@ -1,6 +1,11 @@
+import 'package:cryptzz_app/models/fetch_coin/bid_data.dart';
 import 'package:cryptzz_app/providers/AppState.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import '../../../widgets/crypto_coin_page/widgets.dart';
+
+//_TypeError (type 'BigDataModel' is not a subtype of type 'Future<BigDataModel>?'
 
 class ShowCryptoCoinPage extends StatefulWidget {
   ShowCryptoCoinPage({Key? key}) : super(key: key);
@@ -10,18 +15,38 @@ class ShowCryptoCoinPage extends StatefulWidget {
 }
 
 class _ShowCryptoCoinPageState extends State<ShowCryptoCoinPage> {
+  BigDataModel? data_model;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    // inisialize();
   }
 
-  void inisialize() async {
+  Future<BigDataModel> inisialize() async {
     var app = Provider.of<AppState>(context, listen: false);
+    data_model = await app.getCrypto();
+    print(data_model);
+    return data_model!;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FutureBuilder<BigDataModel>(
+      future: inisialize(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasData) {
+            var coinsData = snapshot.data!.dataModel;
+            return CoinListWidget(coins: coinsData);
+          }
+        } else if (snapshot.hasError) {
+          return Center(child: Text('${snapshot.error}'));
+        }
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
   }
 }
